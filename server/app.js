b441e026790675f783fe7dfdb4c98261d41c0575
KS-Anandh <167880483+KS-Anandh@users.noreply.github.com>
2024-05-23 09:07:14 +0530

    Update app.js

--- server/app.js
+++ server/app.js
@@ -4,7 +4,13 @@ import mongoose from 'mongoose'
 import studentRoute from './routes/studentRoutes.js'
 const app=express()
 app.use(cors())
-app.use(express.json())
+app.use(express.json(
+    {
+        origin:["https://deploy-mern-1whq.vercel.app"],
+        methods:["POST","GET","PUT","DELETE"],
+        credentials:true
+    }
+));
 app.use("/student",studentRoute)
 mongoose.connect("mongodb+srv://KS-Anandh:Nandha1432@cluster0.4rkke6i.mongodb.net/studentInfo?retryWrites=true&w=majority&appName=Cluster0")
 .then(()=>{
